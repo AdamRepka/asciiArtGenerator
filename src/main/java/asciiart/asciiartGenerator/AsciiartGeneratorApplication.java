46f1239def7abc4a31aa05c605859bc0ad9874f1
Řepka Adam <arepka@profinit.eu>
2023-01-07 16:42:10 +0100

    Created IDEA project.

---
+++ src/main/java/asciiart/asciiartGenerator/AsciiartGeneratorApplication.java
@@ -0,0 +1,13 @@
+package asciiart.asciiartGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsciiartGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsciiartGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/asciiart/asciiartGenerator/AsciiartGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package asciiart.asciiartGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsciiartGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
